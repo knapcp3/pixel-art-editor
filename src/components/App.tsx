5fc67991d50b2aa16109b707056859138b80a220
Jakub Knapik <jakubknapik3@gmail.com>
2019-03-02 16:48:06 +0100

    Setup parcel and react

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+class App extends Component {
+  public render() {
+    return (
+      <section className="app-container">
+        XDD
+        {/* <Router>
+          <Switch>
+            <Route path="/login" component={Login} />
+          </Switch>
+        </Router> */}
+      </section>
+    )
+  }
+}
+
+export default App
